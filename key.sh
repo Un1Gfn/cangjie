bd1344f3b5a6191357d85c6a67ee9a60848b4dfb
Darren Ng <Un1Gfn@gmail.com>
2021-11-09 18:24:10 +0800

    build dictionary with gdbm database
    Signed-off-by: Darren Ng <Un1Gfn@gmail.com>

--- key.sh
+++ key.sh
@@ -6,34 +6,16 @@
 
   echo
 
-cat <<"EOF"
-practice keys and radicals of Cangjie input method
-<https://en.wikipedia.org/wiki/Cangjie_input_method#Keys_and_%22radicals%22>
-
-keyboard layout
-page 4 of ~/pdf/5cjbook.pdf
-<https://en.wikipedia.org/wiki/Cangjie_input_method#Keyboard_layout>
-EOF
-
-  echo
-
-  printf "\e[34m%s\e[0m" "press <Q> to quit"
-  echo
-  echo
-
-  printf "\e[2m%s\e[0m" "[mistake 日_A with 斜(竹)_H 人_S 側(尸)_S, then press 日_A correctly]"
+  # printf "\e[2m%s\e[0m" "[mistake 日_A with 斜(竹)_H 人_S 側(尸)_S, then press 日_A correctly]"
   echo
   printf "\e[33m%s \e[0m" "日?"
   printf "\e[31m%s \e[0m" "斜(竹)"
   printf "\e[31m%s \e[0m" "人"
   printf "\e[32m%s \e[0m" "日"
   echo
-  echo
 
-  printf "\e[2m%s\e[0m" "[press Q]"
-  echo
+  # printf "\e[2m%s\e[0m" "[press Q]"
   printf "\e[33m%s \e[0m" "水?"
-  echo "bye"
   echo
 
   # move cursor to bottom
---
+++ serialize.h
@@ -0,0 +1,12 @@
+#ifndef SERIALIZE_H
+#define SERIALIZE_H
+
+#define eprintf(...) fprintf(stderr,__VA_ARGS__)
+
+// radicals per kanji
+#define RPK 5
+
+// ["C"]: list of all characters
+#define C "C"
+
+#endif
