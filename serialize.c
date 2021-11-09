2dac65da49f77e067406222729a1da51eb68902f
Darren Ng <Un1Gfn@gmail.com>
2021-11-09 18:24:10 +0800

    build dictionary with gdbm database
    Signed-off-by: Darren Ng <Un1Gfn@gmail.com>

---
+++ serialize.c
@@ -0,0 +1,150 @@
+// serialize com.github.Jackchows.Cangjie5 to a gdbm database
+
+// instructions in sphinx.public/cangjie.rst
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h> // S_IR* S_IW*
+#include <gdbm.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <wchar.h>
+
+#include "serialize.h"
+
+// max N characters (262144)
+#define CJKS (1<<18)
+
+#define STR0(x) #x
+#define STR(x) STR0(x)
+#define eprintf(...) fprintf(stderr,__VA_ARGS__)
+
+static GDBM_FILE dbf=NULL;
+
+static wint_t c[CJKS+1]={};
+static datum pc={.dptr=(void*)(c),.dsize=0};
+
+static_assert(32/8==sizeof(wint_t));
+static wint_t k=0;
+static const datum pk={.dptr=(void*)(&k),.dsize=sizeof(k)};
+static_assert(32/8==sizeof(wint_t));
+static char v[RPK+1]={};
+static const datum pv={.dptr=v,.dsize=RPK+1};
+
+// https://github.com/coreutils/coreutils/blob/master/src/uname.c
+static void help(const char *const argv0){
+  eprintf("Usage: %s filename.gdbm 0<dictionary.txt\n",argv0);
+  eprintf("       %s --help\n",argv0);
+}
+
+static inline void count(const bool buf){
+  gdbm_count_t ks=0;
+  size_t bs=0;
+  assert(0==gdbm_count(dbf,&ks));
+  assert(0==gdbm_bucket_count(dbf,&bs));
+  buf?
+    printf("%llu keys in %zu buckets\n",ks,bs):
+    eprintf("%llu keys in %zu buckets\n",ks,bs);
+}
+
+static inline bool getline2(){
+
+  // assert(WEOF!=(k=fgetwc(stdin)));
+  const int n=scanf("%lc",&k);
+  if(1!=n){
+    assert(-1==n);
+    assert(!ferror(stdin));
+    assert(feof(stdin));
+    return false;
+  }
+
+  // eprintf("0x%08X [%lc] ",k,k); //
+  assert(1<=k);
+  // printf("%d\n",pc.dsize);
+  c[pc.dsize++]=k;
+
+  bzero(v,RPK+1);
+  assert('\t'==getchar());
+  assert(1==scanf("%"STR(RPK)"s",v)); // (unreliable) whitespace of any kind matches 0+ whitespace of any kind
+  assert('\0'==v[RPK]);
+  // eprintf("\"%s\" ",v); //
+  if('\n'!=getchar())
+    assert(!ferror(stdin)&&feof(stdin));
+
+  // eprintf("\n"); //
+  return true;
+
+}
+
+static inline void add(){
+
+  // count(true);
+
+  // assert(!gdbm_exists(dbf,pk));
+
+  const int n=gdbm_store(dbf,pk,pv,GDBM_INSERT);
+
+  if(0==n){
+
+    // putchar('.');
+
+  }else{
+
+    assert(+1==n);
+    // putchar('O');
+    // printf("\e[7m \e[0m");
+
+    // fetch
+    datum d=gdbm_fetch(dbf,pk);
+    assert(d.dptr);
+    assert(RPK+1<=d.dsize);
+    assert(0==d.dsize%(RPK+1));
+
+    // modify
+    assert((d.dptr=realloc(d.dptr,d.dsize+(RPK+1))));
+    bzero(d.dptr+d.dsize,RPK+1);
+    strcpy(d.dptr+d.dsize,v);
+    d.dsize+=(RPK+1);
+
+    // replace
+    assert(0==gdbm_store(dbf,pk,d,GDBM_REPLACE));
+    free(d.dptr);d.dptr=NULL;
+
+  }
+
+}
+
+int main(const int argc,const char *const argv[]){
+
+  if(true
+    && (0==isatty(STDIN_FILENO)&&ENOTTY==errno) // make sure stdin is not terminal
+    && (2==argc&&0!=strcmp("--help",argv[1]))
+  ){
+    ;
+  }else{
+    help(argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  assert(setlocale(LC_ALL,""));
+  assert(-1==access(argv[1],F_OK)&&ENOENT==errno); // make sure argv[1] does not exist
+  assert((dbf=gdbm_open(argv[1],0,GDBM_WRCREAT,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH,NULL)));
+  count(false);
+
+  while(getline2())
+    add();
+
+  // store an array of all under key C
+  pc.dsize*=sizeof(wint_t);
+  assert(0==gdbm_store(dbf,(datum){.dptr=C,.dsize=strlen(C)},pc,GDBM_REPLACE));
+
+  assert(0==gdbm_reorganize(dbf));
+  count(false);
+  assert(0==gdbm_close(dbf));dbf=NULL;
+  return 0;
+
+}
---
+++ serialize.h
@@ -0,0 +1,10 @@
+#ifndef SERIALIZE_H
+#define SERIALIZE_H
+
+// radicals per kanji
+#define RPK 5
+
+// ["C"]: list of all characters
+#define C "C"
+
+#endif
