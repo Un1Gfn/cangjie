520cf747902b46bc6efd0d3727697db4a97105ca
Darren Ng <Un1Gfn@gmail.com>
2022-10-24 15:39:49 +0800

    Hotspot
    Signed-off-by: Darren Ng <Un1Gfn@gmail.com>

--- src/bin.http/wt88047.srv.cj5.sh
+++ src/bin.http/wt88047.srv.cj5.sh
@@ -1,21 +1,39 @@
-#!/bin/ash
+#!/bin/bash
 
 {
 
-  IPP=192.168.0.136:80
+  # https://askubuntu.com/questions/844913/how-to-control-ip-ranges-of-network-managers-hotspots
+  # https://people.freedesktop.org/~lkundrak/nm-docs/nm-settings.html
+
+  # IPP=192.168.0.136:80 # Tenda
+
+  IPP=10.42.0.1:80 # Hotspot
+
+  # address-data=[{address: '10.42.0.1', prefix: 24}]
+  # address1=10.42.0.1/24,10.42.0.1
+  # gateway=10.42.0.1
+
+  # address-data=[{address: '10.44.0.1', prefix: 24}]
+  # address1=10.44.0.1/24,10.44.0.4
+  # gateway=10.44.0.4
+
   SRV=/srv.cj5
 
   echo "http://$IPP/cgi-bin/cj5.cgi"
 
   cd "$SRV" || exit 1
 
-  sudo /bin/busybox-extras httpd \
-    -f \
-    -vv \
-    -p "$IPP" \
-    -u nobody:nobody \
-    -h "$SRV"
 
+  CMD=(
+    sudo /bin/busybox-extras httpd
+    -f
+    -vv
+    -p "$IPP"
+    -u nobody:nobody
+    -h "$SRV"
     # -c /etc/httpd.conf
+  )
+
+  tmux new -A -s "cj5" "${CMD[*]}"
 
 }; exit
